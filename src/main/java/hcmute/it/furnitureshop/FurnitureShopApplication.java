c98a74ec94a9d9e3b2f947482386ef9df9d87e2a
Lê Quang Duẩn <20110619@student.hcmute.edu.vn>
2023-09-13 21:14:04 +0700

    spring dependencies

---
+++ src/main/java/hcmute/it/furnitureshop/FurnitureShopApplication.java
@@ -0,0 +1,13 @@
+package hcmute.it.furnitureshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurnitureShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurnitureShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hcmute/it/furnitureshop/FurnitureShopApplicationTests.java
@@ -0,0 +1,13 @@
+package hcmute.it.furnitureshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurnitureShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
