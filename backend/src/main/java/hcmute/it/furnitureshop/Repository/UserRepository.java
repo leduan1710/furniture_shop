b83adbc02bd3408b5f4d4fd8d9edfc1702f71150
Lê Quang Duẩn <20110619@student.hcmute.edu.vn>
2023-10-09 09:55:58 +0700

    fix conflictt

--- backend/src/main/java/hcmute/it/furnitureshop/Repository/UserRepository.java
+++ backend/src/main/java/hcmute/it/furnitureshop/Repository/UserRepository.java
@@ -1,8 +1,7 @@
 package hcmute.it.furnitureshop.Repository;
 
 import hcmute.it.furnitureshop.Entity.User;
-<<<<<<< HEAD
-import jakarta.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,12 +10,4 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByUsername(String username);
-=======
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends CrudRepository<User,Integer> {
-    User findByUsername(String username);
->>>>>>> c2fcefc52ed64502f04394fe8d4eae2cf11916c8
 }
