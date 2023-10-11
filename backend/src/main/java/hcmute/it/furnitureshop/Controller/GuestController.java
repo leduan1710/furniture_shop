3a6f1b9199beea9e2895665ed1ccbe1b9fbfef90
unknown <chauthuanphat10@gmail.com>
2023-10-11 21:50:45 +0700

    10/11

--- backend/src/main/java/hcmute/it/furnitureshop/Controller/GuestController.java
+++ backend/src/main/java/hcmute/it/furnitureshop/Controller/GuestController.java
@@ -12,12 +12,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.Optional;
 
 @RestController
-public class PublicController {
+@RequestMapping("/guest")
+public class GuestController {
     @Autowired
     RoomService roomService;
 
--- backend/src/main/java/hcmute/it/furnitureshop/demo/Democontroller.java
+++ backend/src/main/java/hcmute/it/furnitureshop/demo/Democontroller.java
@@ -1,15 +1,20 @@
 package hcmute.it.furnitureshop.demo;
 
+import hcmute.it.furnitureshop.Config.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/demo")
+@RequestMapping("/user/check")
 public class Democontroller {
+    @Autowired
+    JwtService jwtService;
     @GetMapping
     public ResponseEntity<String> sayHello(){
-        return ResponseEntity.ok("Hello from secured endpoint");
+        return ResponseEntity.ok("Hello User");
+        //return jwtService.extractUserName(token);
     }
 }
