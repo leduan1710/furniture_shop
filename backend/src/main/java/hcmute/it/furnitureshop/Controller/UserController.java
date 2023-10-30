f709d0ef1d35ec686da9684eed84a4da8b1db33b
unknown <chauthuanphat10@gmail.com>
2023-10-31 00:25:29 +0700

    update info user,add DTO update user

--- backend/src/main/java/hcmute/it/furnitureshop/Controller/UserController.java
+++ backend/src/main/java/hcmute/it/furnitureshop/Controller/UserController.java
@@ -1,6 +1,7 @@
 package hcmute.it.furnitureshop.Controller;
 
 import hcmute.it.furnitureshop.Config.JwtService;
+import hcmute.it.furnitureshop.DTO.UpdateUserDTO;
 import hcmute.it.furnitureshop.Entity.User;
 import hcmute.it.furnitureshop.Service.Impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -45,11 +46,11 @@ public class UserController {
         return ResponseEntity.status(200).body("Cập nhật thành công");
     }
 
-    @RequestMapping("/saveUser")
-    public void saveUser(@RequestBody User user){
-        Optional<User> userCurrent=userService.findByName(jwtService.extractUserName(getToken()));
-        userCurrent.get().setName(user.getName());
-        userService.saveUser(user);
-
+    @PatchMapping("/saveUser")
+    public void saveUser(@RequestBody UpdateUserDTO userDTO){
+        Optional<User> userOld=userService.findByName(jwtService.extractUserName(getToken()));
+        userOld.get().setName(userDTO.getName());
+        userOld.get().setAddress(userDTO.getAddress());
+        userService.saveUser(userOld.get());
     }
 }
